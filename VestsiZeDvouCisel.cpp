ff4bcaaf88bd284d4acfb0f8a50e8c901f5eb657
kerslager <milan.kerslager@seznam.cz>
2021-10-25 14:00:32 +0200

    Přidejte soubory projektu.

---
+++ VestsiZeDvouCisel.cpp
@@ -0,0 +1,26 @@
+﻿// Program vypíše větší ze dvou čísel
+// - je využita vnořená podmínka a pomocná proměnná max
+
+#include <stdio.h>
+
+int main()
+{
+    // vypíšeme největší číslo
+    int a = 4, b = 4;
+    int max;
+    if (a == b) {
+        // čísla jsou si rovna
+        max = a;
+    }
+    else {
+        if (a > b) {
+            // a je větší
+            max = a;
+        }
+        else {
+            // b je větší
+            max = b;
+        }
+    }
+    printf("nejvetsi je cislo %d", max);
+}
